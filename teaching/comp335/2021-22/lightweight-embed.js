f1d79bcdfbc75db8322d2bd5ff26f908f5ff112a
Sebastian Wild <wild@liverpool.ac.uk>
2022-09-13 00:47:28 +0100

    roll-over to new lecture pages; new photo

---
+++ teaching/comp335/2021-22/lightweight-embed.js
@@ -0,0 +1,45 @@
+/* Light YouTube Embeds by @labnol */
+/* Web: http://labnol.org/?p=27941 */
+
+document.addEventListener("DOMContentLoaded",
+	function() {
+		var div, n,
+			v = document.getElementsByClassName("youtube-player");
+		for (n = 0; n < v.length; n++) {
+			div = document.createElement("div");
+			div.setAttribute("data-id", v[n].dataset.id);
+			if (v[n].hasAttribute("data-opts")) {
+				div.setAttribute("data-opts", "&" + v[n].dataset.opts);
+			} else {
+				div.setAttribute("data-opts", "");
+			}
+			div.innerHTML = labnolThumb(v[n].dataset.id);
+			div.onclick = labnolIframe;
+			v[n].appendChild(div);
+		}
+		v = document.getElementsByClassName("stream-player");
+		for (n = 0; n < v.length; n++) {
+			var iframe = document.createElement("iframe");
+			var embed = "https://stream.liv.ac.uk/s/ID";
+			iframe.setAttribute("src", embed.replace("ID", v[n].dataset.id));
+			iframe.setAttribute("frameborder", "0");
+			iframe.setAttribute("allowfullscreen", "1");
+			v[n].appendChild(iframe);
+		}
+	});
+
+function labnolThumb(id) {
+	var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg"/>',
+		play = '<div class="play"></div>';
+	return thumb.replace("ID", id) + play;
+}
+
+function labnolIframe() {
+	var iframe = document.createElement("iframe");
+	var embed = "https://www.youtube.com/embed/ID?autoplay=1&rel=0ADDITIONALOPTIONS";
+	iframe.setAttribute("src", embed.replace("ID", this.dataset.id).
+		replace("ADDITIONALOPTIONS", this.dataset.opts));
+	iframe.setAttribute("frameborder", "0");
+	iframe.setAttribute("allowfullscreen", "1");
+	this.parentNode.replaceChild(iframe, this);
+}
